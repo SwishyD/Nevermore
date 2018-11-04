10f87156962ae2d7c1b4892fd336524b80ec8299
Unknown <robert.a.nickel@gmail.com>
2018-11-04 15:57:58 +1300

    Basic Pathfinding
    When player tagged object is in range, enemy tracks and speeds up. Will move to the last known location of the player and will return home if it cant find anything

--- Nevermore Unity Project/Assets/basicPathfinding.cs
+++ Nevermore Unity Project/Assets/basicPathfinding.cs
@@ -7,16 +7,21 @@ public class basicPathfinding : MonoBehaviour
 
     //Basic AI
     public GameObject player;
-    float distFromTarget, range = 15, moveSpeed = 2;
+    float distFromTarget, range = 10;
     Vector2 SpawnPos;
     Vector2 VanishPos;
-    bool sightBreak = false;
+    bool sightBreak, lost = false;
+    float timer = 0;
+
+
+    float speed = 0, moveSpeed = .5f, Acceleration, maxSpeed = 5, speedUp = .5f;
+
 
     //Lerping
     Transform playerPos;
     float smoothSpeed = 0.125f;
     Vector3 offset;
-
+    Vector2 speed0Lerp, speedLerp, speed1Lerp;
 
     // Use this for initialization
     void Start()
@@ -24,18 +29,21 @@ public class basicPathfinding : MonoBehaviour
         player = GameObject.FindWithTag("Player");
         SpawnPos = new Vector2(transform.position.x, transform.position.y);
         playerPos = player.transform;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
 
-
-        Vector2 desiredPosition = playerPos.position + offset;
-        Vector2 smoothedPosition = Vector2.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
-
-        print(VanishPos);
+        print(timer);
+        Acceleration = speedUp * timer;
+        speed = moveSpeed + Acceleration;
+        if (speed >= maxSpeed)
+        {
+            speed = maxSpeed;
+        }
+        
 
         //Setting up player and checking distance between player and AI
         if (player != null)
@@ -46,43 +54,81 @@ public class basicPathfinding : MonoBehaviour
         //if the player is within range, begin tracking
         if (distFromTarget < range)
         {
-            Invoke("TrackPlayer", 0.5f);
-        }
-        else if (distFromTarget > range)
-        {
-            sightBreak = true;
-            Home();
+            sightBreak = false;
+            lost = false;
+            TrackPlayer();
         }
 
         if (sightBreak == true)
         {
-            sightBreak = false;
-            VanishPos = new Vector2(player.transform.position.x, player.transform.position.y);
+            Search();
         }
 
+        if (lost == true)
+        {
+            Invoke("Home", 0.5f);
+        }
+        
 
     }
 
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            timer = timer + (1 * Time.deltaTime);
+            sightBreak = false;
+        }
+    }
 
+    //begin timer for multiplying speed
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            timer = timer + (1 * Time.deltaTime);
+            if (timer >= 10)
+            {
+                timer = 10;
+            }
+        }
+    }
 
-
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            VanishPos = new Vector2(player.transform.position.x, player.transform.position.y);
+            sightBreak = true;
+            
+        }
+    }
 
     void TrackPlayer()
     {
-        Vector2 tracking = new Vector2((transform.position.x - VanishPos.x) * moveSpeed, (transform.position.y - VanishPos.y) * moveSpeed);
+        Vector2 tracking = new Vector2((transform.position.x - playerPos.position.x) * speed,   (transform.position.y - playerPos.position.y) * speed);
         GetComponent<Rigidbody2D>().velocity = -tracking;
 
     }
 
-    void Home()
+    void Search()
     {
-        Vector2 searching = new Vector2((transform.position.x - VanishPos.x) * moveSpeed, (transform.position.y - VanishPos.y) * moveSpeed);
+        Vector2 searching = new Vector2((transform.position.x - VanishPos.x) * speed, (transform.position.y - VanishPos.y) * speed);
         GetComponent<Rigidbody2D>().velocity = -searching;
 
-        Vector2 returning = new Vector2((transform.position.x - SpawnPos.x) * moveSpeed, (transform.position.y - SpawnPos.y) * moveSpeed);
-        GetComponent<Rigidbody2D>().velocity = -returning;
+        if(transform.position.x <= VanishPos.x + 1 && transform.position.x >= VanishPos.x - 1 && transform.position.y <= VanishPos.y + 1 && transform.position.y >= VanishPos.y - 1)
+        {
+            sightBreak = false;
+            lost = true;
+        }
+        
+    }
 
-        //Vector2.Lerp(transform.position, playerPos, );
+    void Home()
+    {
+        Vector2 returning = new Vector2((transform.position.x - SpawnPos.x) * speed, (transform.position.y - SpawnPos.y) * speed);
+        GetComponent<Rigidbody2D>().velocity = -returning;
+        timer = 0;
     }
 }
 
