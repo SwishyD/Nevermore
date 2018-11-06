1e7b3d092b2a5f86573af985dd573c904ce8f9fc
Cody <xiswishd@gmail.com>
2018-11-06 15:15:08 +1300

    Movement, attack base

---
+++ Nevermore Unity Project/Assets/Scripts/Cody/EnemyCody.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCody : MonoBehaviour {
+    public int health;
+    public float speed;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        Debug.Log("damage TAKEN !");
+    }
+}
---
+++ Nevermore Unity Project/Assets/Scripts/Cody/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    
+
+
+    //these are for the dash
+    public float dashSpeed;
+    private float dashTime;
+    public float startDashTime;
+    private int direction;
+    
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+    //these are for base movement
+        float speed = 5f;
+        float moveX = 0f;
+        float moveY = 0f;
+        //Movement code
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveY = 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveY = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = 1f;
+        }
+
+        Vector3 moveDir = new Vector3(moveX, moveY).normalized;
+
+        transform.position += moveDir * speed * Time.deltaTime;
+
+        //Dash code
+
+    }
+
+}
