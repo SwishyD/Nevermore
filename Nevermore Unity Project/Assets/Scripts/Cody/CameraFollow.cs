e1b8de1e1d845bcafe2c79203560f1aba1664b91
Cody <xiswishd@gmail.com>
2018-11-16 14:11:21 +1300

    Added firebolt attack

---
+++ Nevermore Unity Project/Assets/Scripts/Cody/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+    }
+
+
+
+
+
+    // Use this for initialization
+    void Start () {
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
