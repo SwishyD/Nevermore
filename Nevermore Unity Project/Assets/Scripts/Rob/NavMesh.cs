4431562817f642c3ec82a30b87a84137e83d53be
Unknown <robert.a.nickel@gmail.com>
2018-11-20 15:28:10 +1300

    Setting Up navmesh
    Navmesh started. Began writing new ai for tracking player and patrolling site

--- Nevermore Unity Project/Assets/Scripts/Rob/NavMesh.cs
+++ Nevermore Unity Project/Assets/Scripts/Rob/NavMesh.cs
@@ -12,34 +12,84 @@ public class NavMesh : MonoBehaviour {
 
 
     public float minX, maxX, minZ, maxZ;
-    float playerDist;
+    float playerDist, maxRange = 0.5f;
     GameObject player;
+
+    float waitTime, startWaitTime = 1;
+
+    Vector3 patrolArea;
 
 	// Use this for initialization
 	void Start ()
     {
         aiChar = this.GetComponent<NavMeshAgent>();
         player = GameObject.FindWithTag("Player");
-	}
+        RandomSite();
+    }
 
     void Update()
     {
+        //runs if player passes null. assigns player on respawn
         if (player == null)
         {
             player = GameObject.FindWithTag("Player");
         }
 
-        if (aiChar != null)
+        playerDist = Vector3.Distance(gameObject.transform.position, player.transform.position);
+
+
+        if (playerDist >= maxRange)
         {
-            SetDestination();
+            Patrol();
+        }
+        else
+        {
+            //SetDestination();
         }
 
-        playerDist = Vector3.Distance(gameObject.transform.position, player.transform.position);
+
     }
 
+    //patrol location based on values specified by dev
+    void Patrol()
+    {
+
+        aiChar.SetDestination(patrolArea);
+
+
+        if (Vector3.Distance(transform.position, patrolArea) <= 0.4f)
+        {
+            StartWaitTimer();
+        }
+    }
+
+    //tracks the player
     void SetDestination()
     {
-        Vector3 patrolArea = new Vector3(Random.Range(minX, maxX), transform.position.y, Random.Range(minZ, maxZ));
+        Vector3 targetVector = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        aiChar.SetDestination(targetVector);
+
+
+    }
+
+    //counts to 0, performs action and resets timer to 1
+    void StartWaitTimer()
+    {
+        if (waitTime <= 0)
+        {
+            RandomSite();
+
+            waitTime = startWaitTime;
+        }
+        else
+        {
+            waitTime -= Time.deltaTime;
+        }
+    }
+
+    void RandomSite()
+    {
+        patrolArea = new Vector3(Random.Range(minX, maxX), transform.position.y, Random.Range(minZ, maxZ));
     }
 
 }
