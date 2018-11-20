a76eee57f2db17c5cb3f442518ce656e7c1f3433
Unknown <robert.a.nickel@gmail.com>
2018-11-20 14:41:10 +1300

    Adjusting to use navMesh AI
    adjusting sandbox scene

---
+++ Nevermore Unity Project/Assets/Scripts/Rob/NavMesh.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMesh : MonoBehaviour {
+
+    [SerializeField]
+    Transform destination;
+
+    NavMeshAgent aiChar;
+
+
+    public float minX, maxX, minZ, maxZ;
+    float playerDist;
+    GameObject player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        aiChar = this.GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player");
+	}
+
+    void Update()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindWithTag("Player");
+        }
+
+        if (aiChar != null)
+        {
+            SetDestination();
+        }
+
+        playerDist = Vector3.Distance(gameObject.transform.position, player.transform.position);
+    }
+
+    void SetDestination()
+    {
+        Vector3 patrolArea = new Vector3(Random.Range(minX, maxX), transform.position.y, Random.Range(minZ, maxZ));
+    }
+
+}
---
+++ Nevermore Unity Project/Assets/Scripts/Rob/posLock.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class posLock : MonoBehaviour {
+
+    //navmesh agent to lock the position of the sprite to
+    public GameObject target;
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = target.transform.position;
+	}
+}
