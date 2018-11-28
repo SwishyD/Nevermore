21defad04c14310220b0290af853da0996074a2a
Unknown <robert.a.nickel@gmail.com>
2018-11-28 15:28:02 +1300

    Navmesh fix
    Navmesh working in main scene

--- Nevermore Unity Project/Assets/Scripts/Rob/damagePlayer.cs
+++ Nevermore Unity Project/Assets/Scripts/Rob/damagePlayer.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class damagePlayer : MonoBehaviour {
 
     public int damage;
+    float dist;
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update()
+    {
+        dist = Vector3.Distance(gameObject.transform.position, player.transform.position);
+    }
 
     public void DealDamage()
     {
-        GameManager.instance.TakeDamage(damage);
+        if (dist < 1)
+        {
+            GameManager.instance.TakeDamage(damage);
+        }
     }
 }
