c4851e77061088f5b691e8327343e82a73abd146
Unknown <robert.a.nickel@gmail.com>
2018-12-04 12:17:02 +1300

    BB functionality
    boil beast working properly with damage dealing, hitbox growth and animations

--- Nevermore Unity Project/Assets/Scripts/Rob/AoEHit.cs
+++ Nevermore Unity Project/Assets/Scripts/Rob/AoEHit.cs
@@ -4,36 +4,23 @@ using UnityEngine;
 
 public class AoEHit : MonoBehaviour {
 
-    float frameCount = 0;
-    float startPos = -1.03f, endPos = -1.35f;
-    bool beginExpand;
+    //float lerpTime, frameCount = 0;
+    //float startTime, endTime;
+    //bool beginExpand;
+    public int damage;
 
     private void Start()
     {
         resetSize();
-        GetComponent<CapsuleCollider>().enabled = false;
+        //endTime = 48f;
+        //startTime = 0;
     }
 
-    void Update()
-    {
-        print(frameCount);
-        if (beginExpand == true)
-        {
-            if (frameCount < 48f)
-            {
-                frameCount += 0.01f;
-            }
-            else if(frameCount >= 48f)
-            {
-                frameCount = 0;
-            }
-        }
-    }
 
-	void enableHitBox()
+    void enableHitBox()
     {
         GetComponent<CapsuleCollider>().enabled = true;
-        beginExpand = true;
+        //beginExpand = true;
     }
 
     void resetSize()
@@ -41,39 +28,57 @@ public class AoEHit : MonoBehaviour {
         GetComponent<CapsuleCollider>().height = 1.68f;
         GetComponent<CapsuleCollider>().radius = 0.84f;
         GetComponent<CapsuleCollider>().direction = 0;
-        //GetComponent<CapsuleCollider>().transform.localPosition = new Vector3(0.33f, 0, -1.03f);
     }
 
-    void expand()
-    {
-
-        /*  a = 
-                GetComponent<CapsuleCollider>().height = 1.68f;
-                GetComponent<CapsuleCollider>().radius = 0.84f;
-                GetComponent<CapsuleCollider>().direction = 0;
-                GetComponent<CapsuleCollider>().transform.localPosition = new Vector3(0.33f, 0, -1.03f);
-
-         *  b = 
-                GetComponent<CapsuleCollider>().height = 2.13f;
-                GetComponent<CapsuleCollider>().radius = 12.17f;
-                GetComponent<CapsuleCollider>().direction = 0;
-                GetComponent<CapsuleCollider>().transform.localPosition = new Vector3(0.33f, 0, -1.35f);
-
-         *  t = 48 frames
-         */
-
-        Mathf.Lerp(GetComponent<CapsuleCollider>().height = 1.68f, GetComponent<CapsuleCollider>().height = 12.3f, frameCount);
-        Mathf.Lerp(GetComponent<CapsuleCollider>().radius = 0.84f, GetComponent<CapsuleCollider>().radius = 2.17f, frameCount);
-        //GetComponent<CapsuleCollider>().transform.localPosition = new Vector3(0, 0, Mathf.Lerp(startPos, endPos, frameCount));
-
-
-    }
 
     void disableHitBox()
     {
-        beginExpand = false;
+        //beginExpand = false;
         resetSize();
         GetComponent<CapsuleCollider>().enabled = false;
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        takeDamage(damage);
+    }
+
+    void takeDamage(int damage)
+    {
+        GameManager.instance.TakeDamage(damage);
+        GetComponent<CapsuleCollider>().enabled = false;
+    }
+
+
+
+    //void Update()
+    //{
+    //    print(endHeight);
+    //    print(startHeight);
+    //    //print(lerpTime);
+    //    if (beginExpand == true)
+    //    {
+    //        if (frameCount < endTime)
+    //        {
+    //            frameCount++;
+    //            expand();
+    //        }
+    //        else if (frameCount >= endTime)
+    //        {
+    //            frameCount = startTime;
+    //        }
+
+    //        lerpTime = Mathf.InverseLerp(startTime, endTime, frameCount);
+    //        print(lerpTime);
+    //    }
+    //}
+
+
+    //void expand()
+    //{
+    //    Mathf.Lerp(startHeight, endHeight, lerpTime);
+    //    Mathf.Lerp(startRad, endRad, lerpTime);
+    //    print("hit");
+
+    //}
 }
