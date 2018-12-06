cdc496fc1b5859bd830c589961fb3bad7119dd33
Unknown <alexeibartlett@gmail.com>
2018-12-06 14:53:21 +1300

    Beginning Cutscene Done, Level 2 Art implemented
    The level 2 art is in there (without navmesh or sorting order), and the starting cutscene is fully done.

---
+++ Nevermore Unity Project/Assets/Scripts/Alex/Cutscene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Cutscene : MonoBehaviour {
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+    }
+
+    // Use this for initialization
+    public void Load()
+    {
+        Cursor.visible = true;
+        SceneManager.LoadScene(2);
+        
+    }
+}
